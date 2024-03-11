8ed7d7a86d692d5df44d308f4ad17d2d6d218936
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-03-12 00:24:56 +0800

    list class view works

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -3,9 +3,9 @@ merchstore URLs
 """
 
 from django.urls import path
-from .views import hello_test
+from .views import hello_test, ItemList, ItemDetail
 
 urlpatterns = [
-    path('hello', hello_test, name='hello_test')
+    path('hello', hello_test, name='hello_test'),
+    path('items', ItemList.as_view(), name='item_list'),
     ]
-
