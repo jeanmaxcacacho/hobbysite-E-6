524addb75c8af60dc7c67219bc6cc532476e17ef
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-03-11 21:35:03 +0800

    models added

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(
+        ProductType,
+        on_delete=models.SET_NULL,
+        )
+    description = models.TextField()
+    # https://stackoverflow.com/questions/23739030/restrict-django-floatfield-to-2-decimal-places
+    # {{ value | floatformat:2 }} when I get around to making the templaes
+    price = models.FloatField()
+    
