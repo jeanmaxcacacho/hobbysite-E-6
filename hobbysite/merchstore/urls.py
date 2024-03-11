25856d88f524881988bda58da4c16746954d3680
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-03-12 00:52:52 +0800

    created testing pipeline for class based views

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -8,4 +8,7 @@ from .views import hello_test, ItemList, ItemDetail
 urlpatterns = [
     path('hello', hello_test, name='hello_test'),
     path('items', ItemList.as_view(), name='item_list'),
+    path('items/<int:pk>', ItemDetail.as_view(), name='item_detail')
     ]
+
+app_name = 'merch_store'
