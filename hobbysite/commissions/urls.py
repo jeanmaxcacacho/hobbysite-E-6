34ade18eeca5788d427f12459eb4494e207c8038
ysaacmesa <ysaac.mesa@student.ateneo.edu>
2024-03-14 14:05:09 +0800

    fixed typo in commissions app urls.py

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -3,6 +3,6 @@ from .views import CommissionListView, CommissionDetailView
 
 urlpatterns =[
  path('list/', CommissionListView.as_view(), name="commission-list"),
- path('detail/<int:pk>', CommissionDetailView.as_view, name="commission-detail")
+ path('detail/<int:pk>', CommissionDetailView.as_view(), name="commission-detail")
 ]
 app_name = "commissions"
