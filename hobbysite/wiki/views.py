c087abc782eccb919baa8a09bb0e92a001a736a9
uwie <louis.binwag@student.ateneo.edu>
2024-03-17 22:32:33 +0800

    Updated onlineWikiApp
    ADDED
    -- Settings.py configuration
    -- Admin panels for categories and wiki articles
    -- Fixed models to fit urls and views
    -- Made base html, extended by articleList.html and articleDetail.html

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -4,9 +4,11 @@ from .models import Article, ArticleCategory
 
 class ArticleList(ListView):
     model = Article
-    template_name = 'wiki/article_list.html'
+    template_name = 'articlesList.html' 
+    context_object_name = 'articles' 
     
 class ArticleDetail(DetailView):
     model = Article
-    template_name = 'wiki/article_detail.html'
+    template_name = 'articleDetails.html'
+    context_object_name = 'article'
 
