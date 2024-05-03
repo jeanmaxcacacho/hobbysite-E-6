fb9e1ad575a5006c89cb8136772e6957d94a08fd
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-03 20:35:50 +0800

    fixed accounts url

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/accounts', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('merchstore/', include('merchstore.urls')), 
     path('wiki/', include('wiki.urls')), 
     path('blog/', include('blog.urls', namespace="blog")), 
