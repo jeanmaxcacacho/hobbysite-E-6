cc1382780871138ba5e5a56364463b4bf322649f
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-03 20:27:12 +0800

    profile model init

--- hobbysite/accounts/models.py
+++ hobbysite/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# required fields of the profile model
+# products bought
+# products sold
+# articles created (for wiki and blog)
+# commissions created
+# commissions joined
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
