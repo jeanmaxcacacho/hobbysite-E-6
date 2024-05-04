e73fcc82742646f38cf92a9cbdd0b261025e293a
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-05 02:11:36 +0800

    views notes

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -9,10 +9,40 @@ from .models import ProductType, Product
 # Detail View: /merchstore/item/1
 
 """
-So I'm guessing that the page /merchstore/items will list down all product types,
-then per product type it will list down all the products that are in it.
+ProductListView
+- list all products in the system regardless of status
+- when logged in, products by the logged in user in a separate group and removed from all products list
+ - so it's there's two lists if somebody is logged in
+- there should be a link that leads to product creation (listing something in the store)
 
-Then all those product bullets are links to itemdetail.html probs.
+ProductDetailView
+- user are not allowed to purchase their own products (so I guess get the same query as the one in ProductListView)
+- form rendered here that is attached to the transaction model
+ - form should be handled by this view also
+- product stock should change accordingly
+- if not logged in and form is valid redirect to login, if logged in redirect to cart view
+ - transactions are only saved if the user is authenticated
+- if stock is 0 then buy button should not be clickable (apparently an HTML attribute)
+- if current user is the owner then there should be an edit button will lead to update view
+
+ProductCreateView
+- All fields should be available
+- product type and status should be a drop down
+- accessible only to logged in users
+
+ProductUpdateView
+- allow updates of all fields except Owner
+- when stock is 0 automatic out of stock otherwise it's available
+- only accessible to logged in users
+
+CartView
+- shows all transactions made by the logged in user as the buyer
+- categorized by product's owner, similar to ProdListView
+
+TransactionListView
+- all transactions as the seller
+- categorized by buyer
+- only accessible to logged in users
 """
 
 class ItemList(ListView):
