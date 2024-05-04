bf1392da641696369201d1373cf0680530b6a8ac
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-04 10:43:35 +0800

    user management and admim, Profile model now in admin

--- hobbysite/accounts/admin.py
+++ hobbysite/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Profile
 
 # Register your models here.
+
+
+class ProfileInLine(admin.StackedInLine):
+    model = Profile
+    can_delete = false
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInLine, ]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
