c14082a6bd62392f0c78bea31c001e2fa24790ff
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-04 11:31:17 +0800

    create ProfileForm ModelForm

--- hobbysite/accounts/forms.py
+++ hobbysite/accounts/forms.py
@@ -1,1 +1,13 @@
 from django import forms
+
+from .models import Profile
+
+class ProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = [
+            "user_name",
+            "user_email"
+        ]
+
