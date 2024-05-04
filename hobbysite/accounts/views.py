69ee514655798eafeffb4c282e5413bf56b09014
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-04 12:10:59 +0800

    init registration form

--- hobbysite/accounts/views.py
+++ hobbysite/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = ""
+    success_url = reverse_lazy("login")
