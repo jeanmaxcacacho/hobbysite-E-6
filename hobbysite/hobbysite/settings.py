72de2fe434fc9b4b54535d4a0c8414ede8d6f8b9
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-04 21:43:30 +0800

    used best practices from ChatGPT

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR/'static',
+    os.path.join(BASE_DIR, "static")
 ]
 
 STATIC_URL = 'static/'
