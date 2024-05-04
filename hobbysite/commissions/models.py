e33cef5c6bd11ed45657ea176191d1fdbe0e2e01
ysaacmesa <ysaac.mesa@student.ateneo.edu>
2024-05-04 21:17:07 +0800

    Added Job and Job application. Edited Commissions

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -3,9 +3,15 @@ from django.db import models
 # Create your models here
 
 class Commission(models.Model):
+    STATUS_CHOICES = [
+        ('O', 'Open'),
+        ('F', 'Full'),
+        ('C', 'Completed'),
+        ('D', 'Discontinued')   
+    ]
     title = models.CharField(max_length=2555)
     description = models.TextField(max_length=255)
-    people_required = models.IntegerField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='O')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -14,21 +20,26 @@ class Commission(models.Model):
     class Meta: 
         ordering = ['created_on']
 
-class Comment(models.Model):
-    entry = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
+class Job(models.Model):
+    role = models.TextField()
+    people_required = models.IntegerField()
     commission = models.ForeignKey(
         'Commission',
         on_delete=models.CASCADE,
-        related_name='comment'
+        related_name='commission'
     )
 
     def __str__(self):
-        return self.entry
-    
-    class Meta: 
-        ordering = ['-created_on']
+        return self.role
+
+class Job_Application(models.Model):
+    applied_on=models.DateTimeField(auto_now_add=True),
+    job_application = models.ForeignKey(
+        'Job',
+        on_delete=models.CASCADE,
+        related_name='job_application'
+    )
+
     
 
 
