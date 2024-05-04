4aa08e9eebe8cb1bfaf2e061c4bc04c38aeac687
Pacozabala <paco.zabala@student.ateneo.edu>
2024-05-04 16:35:57 +0800

    removed hello and hello_test to test properly

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -3,7 +3,7 @@ merchstore URLs
 """
 
 from django.urls import path
-from .views import hello_test, ItemList, ItemDetail
+from .views import ItemList, ItemDetail
 
 # List View: /merchstore/items
 # Detail View: /merchstore/item/1
