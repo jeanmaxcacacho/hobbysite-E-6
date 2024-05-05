b8b82f5dda31f206e66c9e5bf545108c35d579a6
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-05-06 00:37:45 +0800

    added actual homepage view url

--- hobbysite/homepage/urls.py
+++ hobbysite/homepage/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import homepage_test
+from .views import homepage_test, homepage
 
 app_name = "homepage"
 
 urlpatterns = [
-    path("home/", homepage_test, name="home"),
+    path("home_test/", homepage_test, name="home_test"),
+    path("home/", homepage, name="home")
 ]
