b97bcf8231e6c96e40fa2f3e9c6b2d8ffdb05b61
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-05-06 00:16:23 +0800

    init actual homepage view

--- hobbysite/homepage/views.py
+++ hobbysite/homepage/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def homepage_test(request):
     return HttpResponse("hello from homepage")
+
+
+@login_required
+def homepage(request):
+    return render(
+        request,
+        "",
+        {
+
+        }
+    )
