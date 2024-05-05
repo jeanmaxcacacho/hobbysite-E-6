f8a112063ff62fab132b527c9422e123f3a87462
uwie <louis.binwag@student.ateneo.edu>
2024-05-05 16:50:30 +0800

    Updated articleDetails.html and Admin.py
    Added comment section to show, and added comment register in Admin panel

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Article, ArticleCategory
+from .models import Article, ArticleCategory, Comment
 
 '''
 Admin User
@@ -21,3 +21,9 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'created_on', 'updated_on')
     list_filter = ('category', 'created_on')
     search_fields = ('title', 'entry')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'article', 'created_on', 'updated_on')
+    list_filter = ('created_on',)
+    search_fields = ('author__username', 'article__title', 'entry')
