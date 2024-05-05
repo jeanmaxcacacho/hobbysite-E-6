5dbe7733dbcd04ac745a4a84504c30fc0d80d2be
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-05 21:53:52 +0800

    connected ProductUpdateView to your to app level urls

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -8,7 +8,7 @@ TransactionView: /merchstore/transactions
 """
 
 from django.urls import path
-from .views import ProductListView, ProductDetailView, ProductCreateView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView
 
 # List View: /merchstore/items
 # Detail View: /merchstore/item/1
@@ -18,6 +18,7 @@ app_name = 'merchstore'
 urlpatterns = [
     path("items", ProductListView.as_view(), name='product_list'),
     path("item/<int:pk>/", ProductDetailView.as_view(), name='product_detail'),
-    path("item/add", ProductCreateView.as_view(), name="product_create")
+    path("item/add", ProductCreateView.as_view(), name="product_create"),
+    path("item/<int:pk>/edit", ProductUpdateView.as_view(), name="product_update"),
     ]
 
