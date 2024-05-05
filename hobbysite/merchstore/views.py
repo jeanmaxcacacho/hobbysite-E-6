d2d27bfb40203e203436af88c5b17f6f94a3d049
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-05 22:07:39 +0800

    removed dubious import

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,4 +1,3 @@
-from typing import Any
 from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
