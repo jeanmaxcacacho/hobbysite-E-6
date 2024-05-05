f912d8c81eaf960ee32a8c7fba6659af7e5b1e7d
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-05-06 00:20:55 +0800

    made homepage view

--- hobbysite/homepage/views.py
+++ hobbysite/homepage/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -12,8 +15,8 @@ def homepage_test(request):
 def homepage(request):
     return render(
         request,
-        "",
+        "homepage/home.html",
         {
-
+            "user": request.user
         }
     )
