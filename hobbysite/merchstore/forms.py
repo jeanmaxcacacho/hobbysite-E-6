e62ed278ba85324c45f1525c212152fbdf19849d
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-05 21:17:26 +0800

    fixed TransactionForm class not inheriting properly and init ProductForm

--- hobbysite/merchstore/forms.py
+++ hobbysite/merchstore/forms.py
@@ -2,9 +2,21 @@ from django import forms
 
 from .models import Product, ProductType, Transaction
 
-class TransactionForm():
+class TransactionForm(forms.ModelForm):
     quantity = forms.IntegerField(min_value=1, label="quantity")
 
     class Meta:
         model = Transaction
         fields = ["quantity"]
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["product_type"].queryset = ProductType.objects.all()
+        self.fields["owner"].widget = forms.HiddenInput()
