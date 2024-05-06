bcd93e3dc08313e33a35834f14299921713b08c1
Pacozabala <paco.zabala@student.ateneo.edu>
2024-05-06 19:27:37 +0800

    fixing up forms and templates

--- hobbysite/blog/forms.py
+++ hobbysite/blog/forms.py
@@ -17,11 +17,11 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = '__all__'
-
+    
     def __init__(self, *args, **kwargs):
-            super(ArticleForm, self).__init__(*args, **kwargs)
-        
-            disabled_fields = ['author','article']
-            for field_name in disabled_fields:
-                self.fields[field_name].disabled = True
+        super(CommentForm, self).__init__(*args, **kwargs)
+    
+        disabled_fields = ['author','article']
+        for field_name in disabled_fields:
+            self.fields[field_name].disabled = True
 
