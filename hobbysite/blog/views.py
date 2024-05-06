e38212708bb4eb391b4cc065e18a383d71fed818
Pacozabala <paco.zabala@student.ateneo.edu>
2024-05-06 22:20:53 +0800

    fixed form post functionality

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, FormMixin
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -24,33 +24,35 @@ class ArticleListView(LoginRequiredMixin, ListView):
 
         return queryset
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(FormMixin, DetailView):
     model = Article
     template_name = 'blog/article_detail.html'
     context_object_name = 'article'
     form_class = CommentForm
 
     def get_success_url(self):
-        return reverse_lazy('blog:article-detail', kwargs={ 'pk': self.object.pk })
+        return reverse_lazy('blog:article-detail', kwargs={'pk': self.object.pk })
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         article = self.get_object()
         author_articles = Article.objects.filter(author=article.author).exclude(pk=article.pk)[:2]
         context['author_articles'] = author_articles
-        context['form'] = self.form_class()
+        context['form'] = self.get_form()
+
         return context
     
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
+        self.object = self.get_object()
+        form = self.get_form()
         if form.is_valid():
             profile = Profile.objects.get(user=request.user)
             form.instance.author = profile
-            form.instance.article = self.get_object()
+            form.instance.article = self.object
             form.save()
-            return redirect(self.get_success_url())
+            return self.form_valid(form)
         else:
-            return self.render_to_response(self.get_context_data(form=form))
+            return self.form_invalid(form)
 
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
