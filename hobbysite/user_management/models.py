8d8a6210b814ce90ad6bbd27f80026135a253734
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-07 17:44:41 +0800

    removed comments

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# required fields of the profile model
-# products bought
-# products sold
-# articles created (for wiki and blog)
-# commissions created
-# commissions joined
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=100)
