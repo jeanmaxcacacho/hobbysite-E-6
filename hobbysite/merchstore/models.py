a60c62a952d14d02ce65a8d67a238441e87a0a8d
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-07 17:14:50 +0800

    removed developer comments

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -5,23 +5,6 @@ from user_management.models import Profile
 
 # Create your models here.
 
-"""
-FINALS SPECS
-
-ProductType (same as midterms)
-Product
- - the midterms fields (name, product type, description, price)
- - Owner (foreign key to Profile model, on delete model cascade)
- - stock (integer)
- - status (character field that can be: available [default], on sale, out of stock [when stock = 0])
-Transaction
- - buyer (foreign key to profile, on delete null)
- - product (foreign key to product, on delete null)
- - amount (integer, amt of the Product bought)
- - status (character field taht can be: on cart, to pay, to ship, to receive, delivered)
- - created on (datetime, gets set when model is created)
-"""
-
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
