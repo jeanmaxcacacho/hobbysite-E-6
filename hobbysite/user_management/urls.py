937b5b9bf7db7def49179560cccb1fc95733e218
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-07 18:24:19 +0800

    added profile update url to user_management urls

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserCreateView, dashboard
+from .views import UserCreateView, ProfileUpdateView, dashboard
 
 app_name = "user_management"
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="register"),
+    path("profile/", ProfileUpdateView.as_view(), name="profile_update"),
     path("dashboard/", dashboard, name="dashboard"),
 ]
