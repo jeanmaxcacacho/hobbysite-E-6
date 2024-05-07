c1b73b8ba6054eec60a52b6a3dab3ab4db48f24e
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-07 17:47:08 +0800

    revised documentation

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+"""
+default display_name is user.username
+default email is user.email
+these are provided in Django's default registration form
+"""
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=100)
@@ -10,8 +17,6 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
     
-
-    # default display_name is user.username
     def save(self, *args, **kwargs):
         if not self.display_name:
             self.display_name = self.user.username
