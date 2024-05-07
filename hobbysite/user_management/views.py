e24b725acbb5f0eb7f70851f9fb785cd0d62ee7b
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-07 18:08:34 +0800

    created profile update view

--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import RegistrationForm
+from .forms import RegistrationForm, ProfileUpdateForm
 from .models import Profile
 
 class UserCreateView(CreateView):
@@ -18,6 +19,17 @@ class UserCreateView(CreateView):
         Profile.objects.create(user=user)
         return redirect(self.success_url)
     
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    form_class = ProfileUpdateForm
+    template_name = "user_management/profile_update.html"
+    success_url = reverse_lazy("homepage:home")
+
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
+
 
 @login_required
 def dashboard(request):
