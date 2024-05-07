28e07085e6130508b127e87c3c78f308047132e9
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-05-07 17:41:42 +0800

    refactored Profile model

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -10,7 +10,16 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=100)
+    email = models.EmailField()
     bio = models.TextField(blank=True)
 
     def __str__(self):
         return self.user.username
+    
+
+    # default display_name is user.username
+    def save(self, *args, **kwargs):
+        if not self.display_name:
+            self.display_name = self.user.username
+        super().save(*args, **kwargs)
